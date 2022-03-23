db64a07e7a03b92bea9bc60aba188cb843678c7e
IT21316272 <99665491+IT21316272@users.noreply.github.com>
2022-03-23 07:42:25 +0000

    Tutorial7 Exercise1 completed

--- Item.cpp
+++ Item.cpp
@@ -1,10 +1,46 @@
-#include "Item.h"
+/* #include "Item.h"
 #include <iostream>
 using namespace std;
 
 // 4. Implement Default Constructor Implementation
 // 5. Implement Overloaded Constructor Implementation
 // 6. Implement Destructor (display "Destructor Called")
+
+void Item::setDiscount(float pdiscount) {
+  discount = pdiscount;
+}
+
+float Item::getDiscount() {
+  return discount;
+}
+
+float Item::discountedPrice() {
+   return unitPrice - unitPrice * discount/100;
+}
+
+void Item::display() {
+  cout << "Item : " << itemCode << endl;
+  cout << "Discounted Price " << discountedPrice() << endl;
+} */
+
+#include "Item.h"
+#include <iostream>
+using namespace std;
+
+// 4. Implement Default Constructor Implementation
+Item::Item(){
+  itemCode = 0;
+  unitPrice = 0;
+}
+// 5. Implement Overloaded Constructor Implementation
+Item::Item(int Code, float Price){
+  itemCode = Code;
+  unitPrice = Price;
+}
+// 6. Implement Destructor (display "Destructor Called")
+Item::~Item(){
+  cout<<"Destructor Called"<<endl;
+}
 
 void Item::setDiscount(float pdiscount) {
   discount = pdiscount;
--- Item.h
+++ Item.h
@@ -1,4 +1,4 @@
-// Item.h
+/* // Item.h
 
 class Item {
    private:
@@ -9,6 +9,27 @@ class Item {
       // 1. Write the Default Constructor
       // 2. Write the Overloaded Constructor
       // 3. Write the Destructor
+
+      void setDiscount(float punitPrice);
+      float getDiscount();
+      float discountedPrice();
+      void display();
+}; */
+
+// Item.h
+
+class Item {
+   private:
+      int itemCode;
+      float unitPrice;
+      float discount; // out of 100 e.g. discount = 15
+   public:
+      // 1. Write the Default Constructor
+      Item();
+      // 2. Write the Overloaded Constructor
+      Item(int itemCode, float unitPrice);
+      // 3. Write the Destructor
+      ~Item();
 
       void setDiscount(float punitPrice);
       float getDiscount();
